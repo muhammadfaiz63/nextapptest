72970665d02c69cf726546352dfa840500f73574
muhammadfaiz63 <muhammadfaiz7130@gmail.com>
2021-07-05 02:04:14 +0700

    next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,46 @@
-module.exports = {
-  webpack5: true,
-  reactStrictMode: true,
-}
+const withPlugins = require('next-compose-plugins');
+
+require('dotenv').config();
+const dotenv = require('dotenv');
+const buf = Buffer.from('dotenv ready');
+const opt = { debug: true };
+dotenv.parse(buf, opt);
+
+const localSubPath = {
+	en: 'en',
+	id: 'id',
+};
+
+// Bundle analyzer
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.NODE_ENV !== 'production' && process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+	webpack(config, { dev }) {
+		if (dev && process.env.NODE_ENV !== 'production') {
+			config.devtool = 'cheap-module-source-map';
+		}
+
+		config.module.rules.push({
+			test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+			use: {
+				loader: 'url-loader',
+				options: {
+					limit: 100000,
+				},
+			},
+		});
+
+		return config;
+	},
+
+	env: {
+	},
+	publicRuntimeConfig: {
+		localeSubpaths: localSubPath,
+	},
+	compress: true,
+};
+
+module.exports = withPlugins([[withBundleAnalyzer]], nextConfig);
