5c3eab81a0dd9fc4e421eaf19c512b9c45dc85de
muhammadfaiz63 <muhammadfaiz7130@gmail.com>
2021-07-05 02:24:34 +0700

    push

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
+require('dotenv').config()
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const port = process.env.port
+
+console.log("port",port)
 
 app.prepare().then(() => {
   createServer((req, res) => {
@@ -20,8 +24,8 @@ app.prepare().then(() => {
     } else {
       handle(req, res, parsedUrl)
     }
-  }).listen(3000, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:'+port)
   })
 })
